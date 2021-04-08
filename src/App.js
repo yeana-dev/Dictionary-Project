92357450e4ba350825dc37c0f9b79d2004abf490
yeana-dev <yeana.dev@gmail.com>
2021-04-08 12:18:29 -0500

    Created search form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Dictionary</header>
+      <div className="container">
+        <header>Dictionary</header>
+        <main>
+          <Dictionary />
+        </main>
+      </div>
+      <footer>
+        <a
+          href="https://github.com/yeana-dev/dictionary-project"
+          target="__blank"
+        >
+          Open-sourced code
+        </a>{" "}
+        by Yeana Cho 🌸
+      </footer>
     </div>
   );
 }
