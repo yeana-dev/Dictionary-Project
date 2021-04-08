e2c5f573d932ae1d45ca31c6b8840fea1e3a5680
yeana-dev <yeana.dev@gmail.com>
2021-04-08 14:33:01 -0500

    Integrated search engine
    still little confused about map function. I probably need to watch videos thousands more time.

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 import React, { useState } from "react";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   function search(event) {
@@ -24,6 +26,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" autoFocus={true} onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
