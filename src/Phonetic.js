dbf46eda2ce6483702a14de876bb56e8b77add4f
yeana-dev <yeana.dev@gmail.com>
2021-04-10 14:32:33 -0500

    Interface edited

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,13 +1,13 @@
 import React from "react";
+import "./Phonetic.css";
 
 export default function Phonetic(props) {
   return (
     <div className="Phonetic">
-      <a href={props.phonetic.audio} target="_blank">
+      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
         Listen
       </a>
-      <br />
-      {props.phonetic.text}
+      <span className="text">{props.phonetic.text}</span>
     </div>
   );
 }
--- src/Results.js
+++ src/Results.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Meaning from "./Meaning";
 import Phonetic from "./Phonetic";
+import "./Results.css";
 
 export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
         <section>
-          <h2>{props.results.word}</h2>
+          <p className="word">{props.results.word}</p>
           {props.results.phonetics.map(function (phonetic, index) {
             return (
               <div key={index}>
